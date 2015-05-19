6773acc505c306f7cbef23f876120df21ede95ff
Kang <wandongk@gmail.com>
2015-05-19 21:38:44 +0900

    Slack Push

--- src/main/java/iingyeo/IingyeoApplication.java
+++ src/main/java/iingyeo/IingyeoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IingyeoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(IingyeoApplication.class, args);
     }
 }
