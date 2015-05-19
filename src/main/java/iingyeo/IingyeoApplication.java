e69651680f6328187a2a30cabd5649b03df6cf51
kang1224 <Kang@dongwan-kang-ui-MacBook-Pro.local>
2015-05-19 21:07:20 +0900

    Initial commit

---
+++ src/main/java/iingyeo/IingyeoApplication.java
@@ -0,0 +1,12 @@
+package iingyeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IingyeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IingyeoApplication.class, args);
+    }
+}
