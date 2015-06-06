cc1e72701a81f8563284ef58037960cb8f4c8df3
Taemyung Heo <tmheo74@gmail.com>
2015-06-06 14:37:17 +0900

    apply embedmongo-spring for unit test

--- src/main/java/iingyeo/IingyeoApplication.java
+++ src/main/java/iingyeo/IingyeoApplication.java
@@ -1,41 +1,15 @@
 package iingyeo;
 
-import iingyeo.entity.User;
-import iingyeo.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Date;
-
 @SpringBootApplication
-public class IingyeoApplication implements CommandLineRunner {
+public class IingyeoApplication {
 
-    @Autowired
-    private UserRepository userRepository;
 
     public static void main(String[] args) {
-
         SpringApplication.run(IingyeoApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
 
-        userRepository.deleteAll();
-
-        userRepository.save(new User("taemyung", "heo", "tmheo74@gmail.com", new Date()));
-        userRepository.save(new User("minseo", "heo", "minseo@gmail.com", new Date()));
-        userRepository.save(new User("jungwoo", "heo", "jungwoo@gmail.com", new Date()));
-
-        System.out.println("Users found with findAll():");
-        System.out.println("-------------------------------");
-        for (User user : userRepository.findAll()) {
-            System.out.println(user);
-        }
-
-        System.out.println();
-        System.out.println(userRepository.findByEmailAddress("tmheo74@gmail.com"));
-    }
 }
--- src/test/java/iingyeo/IingyeoApplicationTests.java
+++ src/test/java/iingyeo/IingyeoApplicationTests.java
@@ -1,18 +1,61 @@
 package iingyeo;
 
+import iingyeo.entity.User;
+import iingyeo.repository.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = IingyeoApplication.class)
+@SpringApplicationConfiguration(classes = IingyeoTestApplication.class)
 @WebAppConfiguration
 public class IingyeoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    private static final Logger log = LoggerFactory.getLogger(IingyeoApplicationTests.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() throws Exception {
+
+        userRepository.deleteAll();
+
+    }
+
+    @Test
+    public void testFindByEmailAddress() {
+
+        // Given
+        userRepository.save(new User("taemyung", "heo", "tmheo74@gmail.com", new Date()));
+        userRepository.save(new User("minseo", "heo", "minseo@gmail.com", new Date()));
+        userRepository.save(new User("jungwoo", "heo", "jungwoo@gmail.com", new Date()));
+
+        for (User user : userRepository.findAll()) {
+            log.info("user : {}", user);
+        }
+
+        // When
+        User user = userRepository.findByEmailAddress("tmheo74@gmail.com");
+
+        log.info("find user by emailAddress : {}", user);
+
+        // Then
+        assertThat(user, notNullValue());
+        assertThat(user.getFirstName(), is("taemyung"));
+
+    }
 
 }
