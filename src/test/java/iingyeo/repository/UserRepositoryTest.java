1d4df89d7f4f8bf989518910fcc890345399674e
Taemyung Heo <tmheo74@gmail.com>
2015-06-09 20:55:17 +0900

    fix failed test

--- src/test/java/iingyeo/repository/UserRepositoryTest.java
+++ src/test/java/iingyeo/repository/UserRepositoryTest.java
@@ -2,13 +2,10 @@ package iingyeo.repository;
 
 import iingyeo.IingyeoTestApplication;
 import iingyeo.entity.User;
-import iingyeo.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -22,6 +19,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = IingyeoTestApplication.class)
+@WebAppConfiguration
 @Slf4j
 public class UserRepositoryTest {
 
