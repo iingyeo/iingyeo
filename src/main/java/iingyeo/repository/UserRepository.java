7f93172f1202ec490847c5c8b81d78fc373eda18
Taemyung Heo <tmheo74@gmail.com>
2015-06-13 00:44:34 +0900

    #24 apply querydsl-mongodb

--- src/main/java/iingyeo/repository/UserRepository.java
+++ src/main/java/iingyeo/repository/UserRepository.java
@@ -2,11 +2,12 @@ package iingyeo.repository;
 
 import iingyeo.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
 /**
  * Created by Taemyung on 2015-06-06.
  */
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String>, QueryDslPredicateExecutor<User> {
 
     public User findByEmailAddress(String emailAddress);
 
