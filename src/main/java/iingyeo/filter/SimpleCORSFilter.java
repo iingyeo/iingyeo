f48a2daadd7da8aaedefff596412108ce8b02434
taemyung <tmheo74@gmail.com>
2015-09-28 22:14:52 +0900

    - #56 set initial value for reply, like count

--- src/main/java/iingyeo/filter/SimpleCORSFilter.java
+++ src/main/java/iingyeo/filter/SimpleCORSFilter.java
@@ -19,7 +19,7 @@ public class SimpleCORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:1337");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Authorization, Bearer");
 
